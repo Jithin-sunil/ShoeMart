10304cbaf6cb308f04e3816cc77515c9e74b6f2f
jithin-sunil <jithinsunil2003@gmail.com>
2024-09-20 09:54:44 +0530

    template

--- Project/Shop/Foot.php
+++ Project/Shop/Foot.php
@@ -23,6 +23,7 @@
         $(document).ready(function () {
             $(".fancybox").fancybox({
                 openEffect: "none",
+                
                 closeEffect: "none"
             });
 
